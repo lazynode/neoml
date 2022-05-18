13dbf466712522b077237721dffdd25c04ef74aa
dusmart <dusmart@qq.com>
2022-05-18 22:57:23 +0800

    fix test

--- Test.cs
+++ Test.cs
@@ -10,10 +10,10 @@ public class Test
     [Fact]
     public void TestOutput() => Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.GetDirectories("examples").First().GetFiles().Select(v => XElement.Load(v.OpenRead())).ToList().ForEach(v =>
     {
-        using ApplicationEngine engine = ApplicationEngine.Run(v.compile().finalize(), new NeoSystem(ProtocolSettings.Default, null, null).GetSnapshot().CreateSnapshot(), container: null, settings: ProtocolSettings.Default, gas: ApplicationEngine.TestModeGas);
+        using ApplicationEngine engine = ApplicationEngine.Run(v.finalize(), new NeoSystem(ProtocolSettings.Default, null, null).GetSnapshot().CreateSnapshot(), container: null, settings: ProtocolSettings.Default, gas: ApplicationEngine.TestModeGas);
         Assert.Equal(VMState.HALT, engine.State);
         Assert.Equal(v.attr("result") ?? "", engine.ResultStack.Peek().ToJson().ToString());
     });
     [Fact]
-    public void TestBytecode() => Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.GetDirectories("examples").First().GetFiles().Select(v => XElement.Load(v.OpenRead())).ToList().ForEach(v => Assert.Equal(Convert.FromBase64String(v.attr("bytecode") ?? ""), v.compile().finalize()));
+    public void TestBytecode() => Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.GetDirectories("examples").First().GetFiles().Select(v => XElement.Load(v.OpenRead())).ToList().ForEach(v => Assert.Equal(Convert.FromBase64String(v.attr("bytecode") ?? "").ToHexString(), v.finalize().ToHexString()));
 }
