412472ab573c23a5ef13e5e496b43f809a89f128
dusmart <dusmart@qq.com>
2022-05-18 16:56:36 +0800

    add result test

--- Test.cs
+++ Test.cs
@@ -1,9 +1,19 @@
 using Xunit;
 using System.Xml.Linq;
 using neoml;
+using Neo.SmartContract;
+using Neo.VM;
+using Neo;
 
 public class Test
 {
     [Fact]
-    public void TestAllExamples() => Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.GetDirectories("examples").First().GetFiles().Select(v => XElement.Load(v.OpenRead())).ToList().ForEach(v => Assert.Equal(v.compile().finalize(), Convert.FromBase64String(v.attr("bytecode")??"")));
+    public void TestOutput() => Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.GetDirectories("examples").First().GetFiles().Select(v => XElement.Load(v.OpenRead())).ToList().ForEach(v =>
+    {
+        using ApplicationEngine engine = ApplicationEngine.Run(v.compile().finalize(), new NeoSystem(ProtocolSettings.Default, null, null).GetSnapshot().CreateSnapshot(), container: null, settings: ProtocolSettings.Default, gas: ApplicationEngine.TestModeGas);
+        Assert.Equal(VMState.HALT, engine.State);
+        Assert.Equal(v.attr("result") ?? "", engine.ResultStack.Peek().ToJson().ToString());
+    });
+    [Fact]
+    public void TestBytecode() => Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.GetDirectories("examples").First().GetFiles().Select(v => XElement.Load(v.OpenRead())).ToList().ForEach(v => Assert.Equal(Convert.FromBase64String(v.attr("bytecode") ?? ""), v.compile().finalize()));
 }
