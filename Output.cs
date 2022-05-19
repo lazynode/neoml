016d2118a5a28f363271652840b9e733ce200ae4
vang1ong7ang <i@7.day>
2022-05-19 06:32:56 +0000

    dev

--- Output.cs
+++ Output.cs
@@ -9,20 +9,20 @@ namespace neoml;
 static class Output
 {
     public static Dictionary<string, string> TYPEFIX = new() { { "int", "Integer" }, { "integer", "Integer" }, { "Integer", "Integer" }, { "bool", "Boolean" }, { "boolean", "Boolean" }, { "Boolean", "Boolean" }, { "null", "Any" }, { "any", "Any" }, { "Any", "Any" }, { "bytes", "ByteArray" }, { "bytearray", "ByteArray" }, { "ByteArray", "ByteArray" }, { "string", "String" }, { "String", "String" }, { "Hash160", "Hash160" }, { "hash160", "Hash160" }, { "Hash256", "Hash256" }, { "hash256", "Hash256" }, { "publickey", "PublicKey" }, { "PublicKey", "PublicKey" }, { "signature", "Signature" }, { "Signature", "Signature" }, { "array", "Array" }, { "Array", "Array" }, { "map", "Map" }, { "Map", "Map" }, { "interopinterface", "InteropInterface" }, { "InteropInterface", "InteropInterface" }, { "void", "Void" }, { "Void", "Void" } };
-    public static byte[] finalize(this XElement node) => node.DescendantsAndSelf().Aggregate(Enumerable.Empty<byte>(), (sb, v) => sb.withassert(v.Name.NamespaceName.Length == 0).emit(v)).ToArray();
-    public static string meta(this XElement node, string key, string def = "") => node.filter("meta").SingleOrDefault()?.a(key) ?? def;
-    public static JString[] supportedstandards(this XElement node) => node.filter("std").Select(v => (JString)v.a("std")!).ToArray();
-    public static JObject abi(this XElement node) => new JObject { ["methods"] = node.filter("func").Select(v => v.func()).ToArray(), ["events"] = node.filter("event").Select(v => v.evt()).ToArray() };
-    public static JObject func(this XElement node) => new JObject { ["name"] = node.a("name"), ["offset"] = node.position(), ["safe"] = node.a("safe")?.pipe(bool.Parse) ?? false, ["returntype"] = node.a("return")!.pipe(v => TYPEFIX[v]), ["parameters"] = node.parameters() };
-    public static JObject evt(this XElement node) => new JObject { ["name"] = node.a("name"), ["parameters"] = node.parameters() };
-    public static int position(this XElement node) => node.root().DescendantsAndSelf().TakeWhile(v => v != node).Select(v => v.size()).Sum();
-    public static JObject[] parameters(this XElement node) => node.filter("arg").Select(v => new JObject { ["name"] = v.a("name"), ["type"] = v.a("type")!.pipe(v => TYPEFIX[v]) }).ToArray();
-    public static JObject[] permissions(this XElement node) => new JObject[] { new JObject { ["contract"] = "*", ["methods"] = "*" } }; // TODO: IMPL
-    public static JObject[] trusts(this XElement node) => new JObject[] { }; //TODO: IMPL
-    public static JObject extra(this XElement node) => node.filter("meta").SingleOrDefault()?.Value?.pipe(v => JObject.Parse(v)) ?? new JObject();
-    public static MethodToken[] methodtokens(this XElement node) => new MethodToken[] { }; // TODO: IMPL
-    public static byte[] nef(this XElement node) => new NefFile() { Compiler = node.meta("compiler", "neoml"), Source = node.meta("src"), Tokens = node.methodtokens(), Script = node.finalize() }.with(v => { v.CheckSum = NefFile.ComputeChecksum(v); }).ToArray();
-    public static string manifest(this XElement node) => new JObject() { ["name"] = node.meta("name"), ["groups"] = new JArray(), ["features"] = new JObject(), ["supportedstandards"] = node.supportedstandards(), ["abi"] = node.abi(), ["permissions"] = node.permissions(), ["trusts"] = node.trusts(), ["extra"] = node.extra() }.ToString();
+    public static byte[] finalize(this XElement x) => x.DescendantsAndSelf().Aggregate(Enumerable.Empty<byte>(), (sb, v) => sb.withassert(v.Name.NamespaceName.Length == 0).emit(v)).ToArray();
+    public static string meta(this XElement x, string key, string def = "") => x.filter("meta").SingleOrDefault()?.a(key) ?? def;
+    public static JString[] supportedstandards(this XElement x) => x.filter("std").Select(v => (JString)v.a("std")!).ToArray();
+    public static JObject abi(this XElement x) => new JObject { ["methods"] = x.filter("func").Select(v => v.func()).ToArray(), ["events"] = x.filter("event").Select(v => v.evt()).ToArray() };
+    public static JObject func(this XElement x) => new JObject { ["name"] = x.a("name"), ["offset"] = x.position(), ["safe"] = x.a("safe")?.pipe(bool.Parse) ?? false, ["returntype"] = x.a("return")!.pipe(v => TYPEFIX[v]), ["parameters"] = x.parameters() };
+    public static JObject evt(this XElement x) => new JObject { ["name"] = x.a("name"), ["parameters"] = x.parameters() };
+    public static int position(this XElement x) => x.root().DescendantsAndSelf().TakeWhile(v => v != x).Select(v => v.size()).Sum();
+    public static JObject[] parameters(this XElement x) => x.filter("arg").Select(v => new JObject { ["name"] = v.a("name"), ["type"] = v.a("type")!.pipe(v => TYPEFIX[v]) }).ToArray();
+    public static JObject[] permissions(this XElement x) => new JObject[] { new JObject { ["contract"] = "*", ["methods"] = "*" } }; // TODO: IMPL
+    public static JObject[] trusts(this XElement x) => new JObject[] { }; //TODO: IMPL
+    public static JObject extra(this XElement x) => x.filter("meta").SingleOrDefault()?.Value?.pipe(v => JObject.Parse(v)) ?? new JObject();
+    public static MethodToken[] methodtokens(this XElement x) => new MethodToken[] { }; // TODO: IMPL
+    public static byte[] nef(this XElement x) => new NefFile() { Compiler = x.meta("compiler", "neoml"), Source = x.meta("src"), Tokens = x.methodtokens(), Script = x.finalize() }.with(v => { v.CheckSum = NefFile.ComputeChecksum(v); }).ToArray();
+    public static string manifest(this XElement x) => new JObject() { ["name"] = x.meta("name"), ["groups"] = new JArray(), ["features"] = new JObject(), ["supportedstandards"] = x.supportedstandards(), ["abi"] = x.abi(), ["permissions"] = x.permissions(), ["trusts"] = x.trusts(), ["extra"] = x.extra() }.ToString();
     public static IEnumerable<byte> emit(this IEnumerable<byte> sb, XElement node)
     {
         switch (node.Name.LocalName)
