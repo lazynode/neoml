1f280e93912061fc661a9088ee80286f774fbe7e
vang1ong7ang <i@7.day>
2022-05-19 08:07:19 +0000

    dev

--- Compiler.cs
+++ Compiler.cs
@@ -5,7 +5,7 @@ namespace neoml;
 static class Compiler
 {
     public static Dictionary<string, Type> LANGUAGES = Assembly.GetAssembly(typeof(Compiler))!.GetTypes().Where(v => v.Namespace == "neoml.language").ToDictionary(v => v.Name.ToUpper());
-    public static XElement compile(this XElement x) => x.with(childrencompile).withif(x.Name.NamespaceName.Any(), selfcompile);
+    public static XElement compile(this XElement x) => x.withif(x.Name.NamespaceName.Any(), selfcompile).with(childrencompile);
     public static void childrencompile(this XElement x) => x.Elements().ToList().ForEach(v => v.compile());
     public static void selfcompile(this XElement x) => LANGUAGES[x.Name.NamespaceName.ToUpper()].GetMethod(x.Name.LocalName.ToUpper())!.Invoke(null, new object?[] { x });
     public static XElement set(this XElement x, XName name, object? val) => x.with(v => v.SetAttributeValue(name, val));
