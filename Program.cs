c8f437fcfb2503a894616d7d7e9064458b06b8a9
vang1ong7ang <i@7.day>
2022-06-16 18:32:47 +0000

    dev

--- Program.cs
+++ Program.cs
@@ -1,28 +1,4 @@
 ﻿using System.Xml.Linq;
-using Neo;
 using neoml;
 
-switch (Environment.GetEnvironmentVariable("OUTPUT"))
-{
-    case "DEBUG":
-        Console.OpenStandardInput().pipe(XElement.Load).compile().print();
-        break;
-    case "BIN":
-        Console.OpenStandardInput().pipe(XElement.Load).compile().finalize().write();
-        break;
-    case "NEF":
-        Console.OpenStandardInput().pipe(XElement.Load).compile().nef().write();
-        break;
-    case "MANIFEST":
-        Console.OpenStandardInput().pipe(XElement.Load).compile().manifest().print();
-        break;
-    case "BASE64":
-        Console.OpenStandardInput().pipe(XElement.Load).compile().finalize().pipe(Convert.ToBase64String).print();
-        break;
-    case "HEX":
-    case null:
-        Console.OpenStandardInput().pipe(XElement.Load).compile().finalize().ToHexString().print();
-        break;
-    default:
-        throw new Exception();
-}
+Console.OpenStandardInput().pipe(XElement.Load).compile().output(Environment.GetEnvironmentVariable("FMT"));
